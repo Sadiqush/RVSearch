64a5105b67350f810af94359f2ee835bcefcbf5f
sadegh <sadegh6khan@protonmail.ch>
2021-01-07 01:33:47 +0330

    first version downloader: download

--- downloader.py
+++ downloader.py
@@ -1,3 +1,31 @@
 import youtube_dl
 
-ydl = youtube_dl.YoutubeDL({'outtmpl': '%(id)s.%(ext)s'})
+
+def get_video(url):
+    _forbid_playlist(url)
+    video = download(url)
+
+
+def download(url):
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'progress_hooks': [_progress]
+    }
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        video = ydl.download([url])
+    return video
+
+
+def _progress(d):
+    if d['status'] == 'finished':
+        print('Done downloading.')
+
+
+def _forbid_playlist(ydl, url):
+    ydl = youtube_dl.YoutubeDL({'outtmpl': '%(id)s.%(ext)s'})
+    with ydl:
+        result = ydl.extract_info(url, download=False)
+
+    if 'entries' in result:
+        raise Exception("Please provide a single video URL.")
+    return None
