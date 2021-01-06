b4ea7fb65d2867db73873055e8112fbae1fb46cf
sadegh <sadegh6khan@protonmail.ch>
2021-01-07 01:50:32 +0330

    first version main

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from reader import read_csv
+from downloader import get_video
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    csv_path = ""
+    source, target = read_csv(csv_path)
+    source_vid = get_video(source)
+    for vid in target:
+        get_video(vid)
