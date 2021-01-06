ed25c41cf5df21c9fc38d70a49d1b64a24140608
sadegh <sadegh6khan@protonmail.ch>
2021-01-07 01:35:27 +0330

    some doc

--- downloader.py
+++ downloader.py
@@ -2,11 +2,13 @@ import youtube_dl
 
 
 def get_video(url):
+    """Does all the process related to download and saving."""
     _forbid_playlist(url)
     video = download(url)
 
 
 def download(url):
+    """Downloads a no-audio mp4 from youtube."""
     ydl_opts = {
         'format': 'bestaudio/best',
         'progress_hooks': [_progress]
@@ -17,11 +19,13 @@ def download(url):
 
 
 def _progress(d):
+    """da progress"""
     if d['status'] == 'finished':
         print('Done downloading.')
 
 
 def _forbid_playlist(ydl, url):
+    """If the URL is a playlist, fuck 'em."""
     ydl = youtube_dl.YoutubeDL({'outtmpl': '%(id)s.%(ext)s'})
     with ydl:
         result = ydl.extract_info(url, download=False)
