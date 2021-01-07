bfaa3dac1fad02dc1b88fb5c163caacb6be9f079
sadegh <sadegh6khan@protonmail.ch>
2021-01-07 21:21:15 +0330

    fixed some stuff

--- video_utils.py
+++ video_utils.py
@@ -1,20 +1,23 @@
 import cv2
 
 from downloader import get_video
+from main import change_path
 
 
 def get_frames(vid_path):
     vid = cv2.VideoCapture(vid_path)
     total_frames = vid.get(7)
-    for i in range(1, int(total_frames)):
+    for i in range(1, int(total_frames), 30):   # 30fps is 30 frames per second.
         vid.set(1, i)
         ret, frame = vid.read()
         cv2.imwrite(f'{vid_path}_frame{i}.jpg', frame)
         print(f'Frame {i+1} saved.')
-        # break
+    vid.release()
     return None
 
 
 if __name__ == "__main__":
+    change_path()
+    # shorter: https://www.youtube.com/watch?v=-XgD-pUFKaI
     video_name = get_video('https://www.youtube.com/watch?v=GpVXn7vswOM')
     get_frames(video_name)
