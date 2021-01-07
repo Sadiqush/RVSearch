f887ccaa3659f2d98c77b46057d9af3570e69bda
sadegh <sadegh6khan@protonmail.ch>
2021-01-07 15:27:44 +0330

    target to target list

--- main.py
+++ main.py
@@ -6,5 +6,4 @@ if __name__ == '__main__':
     csv_path = ""
     source, targets = read_csv(csv_path)
     source_vid = get_video(source)
-    for vid in targets:
-        get_video(vid)
+    target_vids = [get_video(vid) for vid in targets]
