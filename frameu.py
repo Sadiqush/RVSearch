cf7491fba9d117581f6609df621e63de71df6885
sadegh <sadegh6khan@protonmail.ch>
2021-01-07 15:40:31 +0330

    get_frame added

--- frameu.py
+++ frameu.py
@@ -1,1 +1,19 @@
 import cv2
+
+from downloader import get_video
+
+
+def get_frames(vid_path):
+    vid = cv2.VideoCapture(vid_path)
+    total_frames = vid.get(7)
+    for i in range(0, int(total_frames)):
+        vid.set(1, i)
+        ret, still = vid.read()
+        cv2.imwrite(f'{vid_path}_frame{i}.jpg', still)
+        print(f'Frame {i+1} saved.')
+    return None
+
+
+if __name__ == "__main__":
+    video_name = get_video('https://www.youtube.com/watch?v=GpVXn7vswOM')
+    get_frames(video_name)
