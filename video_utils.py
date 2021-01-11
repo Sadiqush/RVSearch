e9b402bb603340c5e53e152078e721868241bdb4
sadegh <sadegh6khan@protonmail.ch>
2021-01-11 21:56:08 +0330

    framing is now only in RAM

--- video_utils.py
+++ video_utils.py
@@ -1,3 +1,5 @@
+from time import time
+
 import cv2
 from skimage.metrics import structural_similarity as ssim
 from skimage.metrics import normalized_root_mse as n_rmse
@@ -15,15 +17,10 @@ def compare_videos(vid1, vid2):
     vid2 = vid2[1]
     tot_fr_1 = vid1.get(7)
     tot_fr_2 = vid2.get(7)
-    print(tot_fr_1)
-    print('end start')
     for i in range(1, int(tot_fr_1), 30):
-        print('enter for')
         frame_1 = get_the_frame(vid1, vid1_name, i)
-        print('got the frame')
         for j in range(1, int(tot_fr_2), 30):
             frame_2 = get_the_frame(vid2, vid2_name, j)
-            print('got the other frame')
             score = compare_frames(frame_1, frame_2)
             if check_score(score):
                 print(f'{vid1_name}_{i} is similar to {vid2_name}_{j}')
@@ -44,32 +41,28 @@ def get_frames(vid, vid_name):
     return None
 
 
-def get_the_frame(vid, vid_name, frm_n):
+def get_the_frame(vid, frm_n):
     """Get the specified frame of a video object, write it on disk."""
     vid.set(1, frm_n)
     ret, frame = vid.read()
-    cv2.imwrite(f'{vid_name}_frame{frm_n}.jpg', frame)
-    # print(f'Frame {frm_n} saved.')
-    return f'{vid_name}_frame{frm_n}.jpg'
+    return frame
 
 
 def compare_frames(image_a, image_b, gray=True, debug=False):
     """Compare two images using SSIM algorithm, return the score."""
-    img1 = cv2.imread(image_a)
-    img2 = cv2.imread(image_b)
-    if img1.shape != img2.shape:
+    if image_a.shape != image_b.shape:
         raise Exception("Not compatible shape to start comparing.")
     if gray:
-        img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
-        img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
-    score = ssim(img1, img2,
+        image_a = cv2.cvtColor(image_a, cv2.COLOR_BGR2GRAY)
+        image_b = cv2.cvtColor(image_b, cv2.COLOR_BGR2GRAY)
+    score = ssim(image_a, image_b,
                  multichannel=True,
                  gaussian_weights=True, sigma=1.5,
                  use_sample_covariance=False)
     if debug:
         # Some other scores, print everything.
-        psnr = cv2.PSNR(img1, img2)
-        nrmse_score = 1 - n_rmse(img1, img2)
+        psnr = cv2.PSNR(image_a, image_b)
+        nrmse_score = 1 - n_rmse(image_a, image_b)
 
         print("SSIM: {}".format(score))
         print('PSNR: ', psnr)
@@ -77,7 +70,7 @@ def compare_frames(image_a, image_b, gray=True, debug=False):
     return score
 
 
-def check_score(score, threshold=0.30):
+def check_score(score, threshold=0.65):
     """Return True if similarity score reaches the threshold."""
     if score >= threshold:
         return True
@@ -86,16 +79,22 @@ def check_score(score, threshold=0.30):
 
 
 if __name__ == "__main__":
+    # TODO: res lower
+    # TODO: GPU accelrate
     change_path()
     # video_name_2 = get_video("https://www.youtube.com/watch?v=-XgD-pUFKaI")
     # video_name = get_video('https://www.youtube.com/watch?v=GpVXn7vswOM')
-    # get_the_frame(video_name, 300)
+    # video1 = load_video(video_name)
+    # get_the_frame(video1[1], video1[0], 300)
     # get_the_frame(video_name_2, 50)
     # compare_frames('GpVXn7vswOM.mp4_frame151.jpg',
     #                'GpVXn7vswOM.mp4_frame150.jpg',
     #                debug=True)
-    video_name_1 = get_video("https://www.youtube.com/watch?v=zTMjucCj590")
-    video_name_2 = get_video("https://www.youtube.com/watch?v=foYWdyACCHE")
-    video1 = load_video(video_name_1)
-    video2 = load_video(video_name_2)
-    compare_videos(video1, video2)
+    # video_name_1 = get_video("https://www.youtube.com/watch?v=zTMjucCj590")
+    # video_name_2 = get_video("https://www.youtube.com/watch?v=foYWdyACCHE")
+    video1 = load_video("GpVXn7vswOM.mp4")
+    # video2 = load_video(video_name_2)
+    # get_the_frame("TwIvUbOhcKE.mp4", 100)
+    start = time()
+    compare_videos(video1, video1)
+    print("--- %s seconds ---" % (time() - start))
