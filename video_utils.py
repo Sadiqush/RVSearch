701654a49c8d7e989c8c5f881c0a32a8a20bd8d4
sadegh <sadegh6khan@protonmail.ch>
2021-01-11 14:59:20 +0330

    added getting single frame, and SSIM comparison

--- video_utils.py
+++ video_utils.py
@@ -1,4 +1,5 @@
 import cv2
+from skimage.metrics import structural_similarity
 
 from downloader import get_video
 from main import change_path
@@ -16,8 +17,28 @@ def get_frames(vid_path):
     return None
 
 
+def get_the_frame(vid_path, frm_n):
+    vid = cv2.VideoCapture(vid_path)
+    vid.set(1, frm_n)
+    ret, frame = vid.read()
+    cv2.imwrite(f'{vid_path}_frame{frm_n}.jpg', frame)
+    print(f'Frame {frm_n} saved.')
+    vid.release()
+    return None
+
+
+def compare_frames(image_a, image_b):
+    img1 = cv2.imread(image_a)
+    img2 = cv2.imread(image_b)
+    image1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+    image2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+    score, diff = structural_similarity(image1, image2, full=True,  multichannel=True)
+    print("SSIM: {}".format(score))
+
+
 if __name__ == "__main__":
     change_path()
     # shorter: https://www.youtube.com/watch?v=-XgD-pUFKaI
     video_name = get_video('https://www.youtube.com/watch?v=GpVXn7vswOM')
-    get_frames(video_name)
+    get_the_frame(video_name, 300)
+    compare_frames('GpVXn7vswOM.mp4_frame300.jpg', 'GpVXn7vswOM.mp4_frame10.jpg')
