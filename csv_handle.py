f7a95ffba7bd5302f94e12f07293ece81d923012
sadegh <sadegh6khan@protonmail.ch>
2021-01-13 22:26:12 +0330

    added init_record() and record_similar()

--- csv_handle.py
+++ csv_handle.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from pathlib import Path
 
 
 def _load_csv(inpath):
@@ -17,3 +18,19 @@ def read_csv(inpath):
     source = df["source"]
     targets = df["target"].tolist()
     return source, targets
+
+
+def init_record_file() -> pd.DataFrame:
+    record_style = {'Source': '',
+                    'Target': '',
+                    'TimeStamp': ''}
+    record_df = pd.DataFrame(record_style)
+    return record_df
+
+
+def record_similarity(record_file: pd.DataFrame, csv_name: str):
+    if Path(csv_name).is_file():
+        record_file.to_csv(f'{csv_name}', index=true)
+    else:
+        record_file.to_csv(f'{csv_name}', index=true)
+    return None
