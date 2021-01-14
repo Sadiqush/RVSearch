ac053909300d475863e21d24351fc45448a6c3bc
sadegh <sadegh6khan@protonmail.ch>
2021-01-14 17:40:21 +0330

    added run

--- main.py
+++ main.py
@@ -11,12 +11,20 @@ def change_path():
     chdir(getcwd() + "/tmp")
 
 
+def run(csv_path):
+    change_path()
+    compilation, sources = read_csv(csv_path)
+    compilation_info = get_video(compilation)
+    sources_info = [get_video(vid) for vid in sources]
+    compilation_vid = load_video(compilation_info)
+    source_vids = load_video(sources_info)
+    record_file = compare_videos(compilation_vid, source_vids)
+    csv_handle.save_csv(record_file)
+
+
 if __name__ == '__main__':
+    run(csv_path="/home/sadegh/video_search_test.csv")
+
     # TODO: read csv
     # TODO: compare
     # TODO: save
-
-    csv_path = ""
-    compilation, sources = read_csv(csv_path)
-    compilation_vid = get_video(compilation)
-    sources_vids = [get_video(vid) for vid in sources]
