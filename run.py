676f7504bf9da2b635f8f4e28a16de5942c923e1
sadegh <sadegh6khan@protonmail.ch>
2021-01-15 20:33:05 +0330

    run.py added

---
+++ run.py
@@ -0,0 +1,11 @@
+import argparse
+from main import run
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('input', help='The csv file to read')
+
+    args = parser.parse_args()
+
+    input_path = args.input
+    run(input_path)
