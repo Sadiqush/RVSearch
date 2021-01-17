f74d5e269305a720c8d0cf10e5b2d74e4b078796
sadegh <sadegh6khan@protonmail.ch>
2021-01-17 15:22:19 +0330

    changed description_content_type

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
     description="GPLv3",
 
     long_description=open("README.md").read(),
-
+    long_description_content_type='text/markdown',
     # Dependent packages (distributions)
     install_requires=[
         "pandas",
