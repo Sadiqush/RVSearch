31ddc38332bdd3cb1ad8c24f199aaab4605aaa1c
sadegh <sadegh6khan@protonmail.ch>
2021-01-21 19:19:29 +0330

    some path changing

--- rvsearch/main.py
+++ rvsearch/main.py
@@ -13,29 +13,33 @@ class MainThread(threading.Thread):
     def __init__(self, qtlog=[]):
         threading.Thread.__init__(self)
         self.qtlog = qtlog
+        self.currnt_path = os.getcwd()
 
-    @staticmethod
-    def change_path():
+    def change_path(self):
         """Change working directory to a temporary directory."""
         dir_of_executable = os.path.dirname(__file__)
-        Path(Path(dir_of_executable) / "tmp/").mkdir(parents=True, exist_ok=True)
-        os.chdir(os.getcwd() + "/tmp")
+        Path(Path(dir_of_executable) / "rvidtmp/").mkdir(parents=True, exist_ok=True)
+        os.chdir(self.currnt_path + "/rvidtmp")
         return None
 
     @staticmethod
     def clean():
         """Remove the tmp directory when you're done"""
         dir_of_executable = os.path.dirname(__file__)
-        Path.rmdir(Path(dir_of_executable) / "tmp/")
+        Path.rmdir(Path(dir_of_executable) / "rvidtmp/")
         return None
 
     def main(self, csv_path, output_path=""):
         """Main function: read csv, download videos, compare them, save results."""
-        currnt_path = os.getcwd()
         self.change_path()
 
+        if not csv_path:  # TODO: run after you didn't put input
+            logger.do_log('You have not provided any input', self.qtlog)
+            self.clean()
+            return None
+
         for csv in csv_path:
-            csv = Path(currnt_path) / csv
+            csv = Path(self.currnt_path) / csv
             com_url, source_list = read_csv(csv)
 
             for source_url in source_list:
@@ -61,9 +65,9 @@ class MainThread(threading.Thread):
 
                 # TODO: maybe save in comparing?
                 if output_path:
-                    final_csv_name = save_csv(record_df, f'{currnt_path}/{output_path}')
+                    final_csv_name = save_csv(record_df, f'{self.currnt_path}/{output_path}')
                 else:
-                    final_csv_name = save_csv(record_df, f'{currnt_path}/{meta_cmp["name"]}_results.csv')
+                    final_csv_name = save_csv(record_df, f'{self.currnt_path}/{meta_cmp["name"]}_results.csv')
                 if not vconf.QUIET: logger.do_log(f'Results saved to {final_csv_name}', self.qtlog)
 
         if not vconf.QUIET: logger.do_log(f'All done. Exiting...', self.qtlog)
